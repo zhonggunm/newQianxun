be260304458d2173a772a171100a0d62a611bb46
zhonggunm@hotmail.com <zhonggunm@hotmail.com>
2020-04-18 10:58:37 -0600

    created

---
+++ qianxun/src/main/java/com/zhong/qianxun/QianxunApplication.java
@@ -0,0 +1,13 @@
+package com.zhong.qianxun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QianxunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QianxunApplication.class, args);
+	}
+
+}
